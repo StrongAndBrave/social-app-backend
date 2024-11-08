01da9a73ac0fe2f59019c9d548e218098b88b941
afdmitriy <af.dmitr@gmail.com>
2024-11-08 22:16:01 +0300

    add global prefix

--- apps/payments-microservice/src/main.ts
+++ apps/payments-microservice/src/main.ts
@@ -3,6 +3,7 @@ import { PaymentsModule } from './payments.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PaymentsModule);
-  await app.listen(process.env.port ?? 3000);
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.port || 3000);
 }
 bootstrap();
