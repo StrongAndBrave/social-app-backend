ca99f1efd6a69f32b0c217a252078bd9c6adc678
springJack <artur0_g@proton.me>
2024-11-24 16:12:15 +0200

    Fixing issue with 502 error.

--- apps/main-gateway/src/main.ts
+++ apps/main-gateway/src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api/v1');
   const PORT = 3630;
+  // TODO: Ask why process.env.port return undefined
   //const PORT = process.env.port
 
   await app.listen(PORT || 3000);
