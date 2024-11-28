9dd147a53a33dba377cb368f9c27762ad4442e04
afdmitriy <af.dmitr@gmail.com>
2024-11-28 16:06:51 +0300

    change PORT

--- apps/main-gateway/src/main.ts
+++ apps/main-gateway/src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  const PORT = 3630;
   // TODO: Ask why process.env.port return undefined
-  //const PORT = process.env.port
-
-  await app.listen(PORT || 3000);
+  const PORT = process.env.PORT
+  console.log('PORT');
+  console.log('App is listening on port', PORT);
+  await app.listen(PORT || 3630);
 }
 bootstrap();
