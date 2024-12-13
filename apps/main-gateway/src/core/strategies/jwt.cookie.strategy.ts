cc9b83c4bec3a875c34353452f6a4b60ce2da1b4
afdmitriy <af.dmitr@gmail.com>
2024-12-13 21:30:31 +0300

    add deployment file

--- apps/main-gateway/src/core/strategies/jwt.cookie.strategy.ts
+++ apps/main-gateway/src/core/strategies/jwt.cookie.strategy.ts
@@ -1,12 +1,13 @@
 import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { jwtConstants } from '../constants/constants';
 import { AuthService } from '../../features/auth/application/auth.service';
+import { AuthConfig } from '../../features/auth/auth.config';
 
 @Injectable()
 export class JwtCookieStrategy extends PassportStrategy(Strategy, 'jwt-cookie') {
-  constructor(@Inject(AuthService.name) private authService: AuthService) {
+  constructor(@Inject(AuthService.name) private authService: AuthService,
+@Inject(AuthConfig.name) private readonly authConfig: AuthConfig) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request) => {
@@ -14,13 +15,13 @@ export class JwtCookieStrategy extends PassportStrategy(Strategy, 'jwt-cookie') 
         },
       ]),
       ignoreExpiration: false,
-      secretOrKey: jwtConstants.secretRefresh,
+      secretOrKey: authConfig.secretRefresh,
     });
   }
 
   async validate(payload: any) {
     // Наверное стоит проверять айди сессии и дату выпуска этой сессии, если у токена дата сессии раньше чем в бд, то не пропускаю
-    const sessionId = await this.authService.sessionIsValid(payload.userId, payload.deviceId, payload.issuedAt);
+    const sessionId = await this.authService.sessionIsValid(payload.deviceId, payload.issuedAt);
     if (!sessionId) throw new HttpException('Unautorized', HttpStatus.UNAUTHORIZED)
     return { userId: payload.userId, deviceId: payload.deviceId };
   }
