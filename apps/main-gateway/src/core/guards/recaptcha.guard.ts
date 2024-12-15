d8d2d9f45d169690d2fd78f3dba8dfd58396bd20
TheBestPetr <ipetrazhitski@gmail.com>
2024-12-15 15:59:17 +0300

    add authConfig to recaptcha

--- apps/main-gateway/src/core/guards/recaptcha.guard.ts
+++ apps/main-gateway/src/core/guards/recaptcha.guard.ts
@@ -1,21 +1,26 @@
 import {
 	CanActivate,
 	ExecutionContext,
+	Inject,
 	Injectable,
 	UnauthorizedException,
 } from '@nestjs/common';
-import * as process from 'node:process';
+import { AuthConfig } from '../../features/auth/auth.config';
 
 @Injectable()
 export class RecaptchaGuard implements CanActivate {
+	constructor(@Inject(AuthConfig.name) private readonly authConfig: AuthConfig) {}
 	async canActivate(context: ExecutionContext): Promise<boolean> {
 		const { body } = await context.switchToHttp().getRequest();
+		if (!body.recaptcha) {
+			throw new UnauthorizedException();
+		}
 		const result = await fetch('https://www.google.com/recaptcha/api/siteverify', {
 			headers: {
 				'Content-Type': 'application/x-www-form-urlencoded',
 			},
 			method: 'POST',
-			body: `secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${body.recaptcha}`,
+			body: `secret=${this.authConfig.recaptchaSecretKey}&response=${body.recaptcha}`,
 		});
 		const response = await result.json();
 		if (!response.success) {
--- apps/main-gateway/src/features/auth/auth.config.ts
+++ apps/main-gateway/src/features/auth/auth.config.ts
@@ -5,43 +5,52 @@ import { IsString } from 'class-validator';
 
 @Injectable()
 export class AuthConfig {
-  @IsString()
-  secretAccess: string;
+	@IsString()
+	secretAccess: string;
 
-  @IsString()
-  secretRefresh: string;
+	@IsString()
+	secretRefresh: string;
 
-  @IsString()
-  accessExpiresIn: string;
+	@IsString()
+	accessExpiresIn: string;
 
-  @IsString()
-  refreshExpiresIn: string;
+	@IsString()
+	refreshExpiresIn: string;
 
-  constructor(private configService: ConfigService) {
-    const secretAccess = this.configService.get('JWT_SECRET_ACCESS');
-    if (secretAccess === undefined) {
-      throw new Error('JWT_SECRET_ACCESS is not defined');
-    }
-    this.secretAccess = secretAccess;
+	@IsString()
+	recaptchaSecretKey: string;
 
-    const secretRefresh = this.configService.get('JWT_SECRET_REFRESH');
-    if (secretRefresh === undefined) {
-      throw new Error('JWT_SECRET_REFRESH is not defined');
-    }
-    this.secretRefresh = secretRefresh;
+	constructor(private configService: ConfigService) {
+		const secretAccess = this.configService.get('JWT_SECRET_ACCESS');
+		if (secretAccess === undefined) {
+			throw new Error('JWT_SECRET_ACCESS is not defined');
+		}
+		this.secretAccess = secretAccess;
 
-    const accessExpiresIn = this.configService.get('JWT_ACCESS_EXPIRES_IN');
-    if (accessExpiresIn === undefined) {
-      throw new Error('JWT_ACCESS_EXPIRES_IN is not defined');
-    }
-    this.accessExpiresIn = accessExpiresIn;
+		const secretRefresh = this.configService.get('JWT_SECRET_REFRESH');
+		if (secretRefresh === undefined) {
+			throw new Error('JWT_SECRET_REFRESH is not defined');
+		}
+		this.secretRefresh = secretRefresh;
 
-    const refreshExpiresIn = this.configService.get('JWT_REFRESH_EXPIRES_IN');
-    if (refreshExpiresIn === undefined) {
-      throw new Error('JWT_REFRESH_EXPIRES_IN is not defined');
-    }
-    this.refreshExpiresIn = refreshExpiresIn;
+		const accessExpiresIn = this.configService.get('JWT_ACCESS_EXPIRES_IN');
+		if (accessExpiresIn === undefined) {
+			throw new Error('JWT_ACCESS_EXPIRES_IN is not defined');
+		}
+		this.accessExpiresIn = accessExpiresIn;
 
-    configValidationUtility.validateConfig(this);
-  }
+		const refreshExpiresIn = this.configService.get('JWT_REFRESH_EXPIRES_IN');
+		if (refreshExpiresIn === undefined) {
+			throw new Error('JWT_REFRESH_EXPIRES_IN is not defined');
+		}
+		this.refreshExpiresIn = refreshExpiresIn;
+
+		const recaptchaSecretKey = this.configService.get('RECAPTCHA_SECRET_KEY');
+		if (recaptchaSecretKey === undefined) {
+			throw new Error('RECAPTCHA_SECRET_KEY is not defined');
+		}
+		this.recaptchaSecretKey = recaptchaSecretKey;
+
+		configValidationUtility.validateConfig(this);
+	}
 }
