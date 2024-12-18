77a248da963692a61b419264a0d6c433d3367eb7
TheBestPetr <ipetrazhitski@gmail.com>
2024-12-19 00:28:20 +0300

    fix path to .env

--- apps/main-gateway/src/config/config-dynamic-module.ts
+++ apps/main-gateway/src/config/config-dynamic-module.ts
@@ -2,13 +2,10 @@ import { ConfigModule } from '@nestjs/config';
 
 // you must import this const in the head of your app.module.ts
 export const configModule = ConfigModule.forRoot({
-  envFilePath: [
-    // TODO: как изменить путь, так чтобы он был внутри папки main-gateway
-    `.env.${process.env.NODE_ENV}.local`,
-    `.env.${process.env.NODE_ENV}`,
-    '.env.production',
-  ],
-  isGlobal: true,
+	envFilePath: [
+		`apps/main-gateway/.env.${process.env.NODE_ENV}.local`,
+		`apps/main-gateway/.env.${process.env.NODE_ENV}`,
+		'apps/main-gateway/.env.production',
+	],
+	isGlobal: true,
 });
-
-
