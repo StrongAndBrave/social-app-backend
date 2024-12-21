ed3b2ae7b46ef7b30506db9b67ca9e0427fe5088
TheBestPetr <ipetrazhitski@gmail.com>
2024-12-21 17:18:36 +0300

    add user model from oauth & decorator

---
+++ apps/main-gateway/src/core/decorators/transform/user-data.oauth.google.ts
@@ -0,0 +1,12 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const CurrentUserDataFromOAuth = createParamDecorator(
+	(data: unknown, context: ExecutionContext) => {
+		const request = context.switchToHttp().getRequest();
+		return {
+			userId: request.user?.id ?? null,
+			tokenKey: request.user?.tokenKey ?? null,
+			deviceId: request.user?.deviceId ?? null,
+		};
+	},
+);
--- apps/main-gateway/src/features/auth/api/models/input/auth.input.models.ts
+++ apps/main-gateway/src/features/auth/api/models/input/auth.input.models.ts
@@ -1,27 +1,37 @@
-import { IsEmail, Length } from "class-validator";
-import { Trim } from "apps/main-gateway/src/core/decorators/transform/trim.decorator";
-import { ConfCodeIsValid } from "apps/main-gateway/src/core/decorators/validate/confirmation-code.decorator";
-import { EmailIsConfirmed } from "apps/main-gateway/src/core/decorators/validate/email-is-confirmed.decorator";
-
+import { IsEmail, IsString, Length } from 'class-validator';
+import { Trim } from 'apps/main-gateway/src/core/decorators/transform/trim.decorator';
+import { ConfCodeIsValid } from 'apps/main-gateway/src/core/decorators/validate/confirmation-code.decorator';
+import { EmailIsConfirmed } from 'apps/main-gateway/src/core/decorators/validate/email-is-confirmed.decorator';
 
 export class ValidationCodeModel {
-  @Trim()
-  @ConfCodeIsValid()
-  code: string;
+	@Trim()
+	@ConfCodeIsValid()
+	code: string;
 }
 
 export class EmailResendingModel {
-  @Trim()
-  @IsEmail()
-  @EmailIsConfirmed()
-  email: string;
+	@Trim()
+	@IsEmail()
+	@EmailIsConfirmed()
+	email: string;
 }
 
-
 export class NewPasswordModel {
-  @Trim()
-  @Length(6, 20)
-  newPassword: string;
-  @Trim()
-  recoveryCode: string
+	@Trim()
+	@Length(6, 20)
+	newPassword: string;
+	@Trim()
+	recoveryCode: string;
+}
+
+export class UserOAuthModel {
+	@Trim()
+	@IsEmail()
+	email: string;
+	@Trim()
+	@IsString()
+	firstName: string;
+	@Trim()
+	@IsString()
+	lastName: string;
 }
