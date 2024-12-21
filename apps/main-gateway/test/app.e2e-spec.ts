b922cf09686519e4783a07a2e14c39ac00aeb80c
afdmitriy <af.dmitr@gmail.com>
2024-12-21 18:01:15 +0300

    Update app.e2e-spec.ts

--- apps/main-gateway/test/app.e2e-spec.ts
+++ apps/main-gateway/test/app.e2e-spec.ts
@@ -1,18 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { configApp } from '../src/config/apply-app-settings/set-app';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    configApp(app);
     await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 
   it('/ (GET)', () => {
