ee53c5d78cb9d2bf0459b9dfb99a96aacad45324
TheBestPetr <ipetrazhitski@gmail.com>
2024-12-26 19:19:55 +0300

    add correct swagger path

--- apps/main-gateway/src/config/apply-app-settings/swagger.setup.ts
+++ apps/main-gateway/src/config/apply-app-settings/swagger.setup.ts
@@ -3,14 +3,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { GLOBAL_PREFIX } from './global-prefix.setup';
 
 export function swaggerSetup(app: INestApplication) {
-  const config = new DocumentBuilder()
-    .setTitle('BLOGGER API')
-    .addBearerAuth()
-    .setVersion('1.0')
-    .build();
+	const config = new DocumentBuilder()
+		.setTitle('BLOGGER API')
+		.addBearerAuth()
+		.setVersion('1.0')
+		.build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(GLOBAL_PREFIX, app, document, {
-    customSiteTitle: 'Blogger Swagger',
-  });
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup(GLOBAL_PREFIX + '/swagger', app, document, {
+		// todo correct path
+		customSiteTitle: 'Blogger Swagger',
+	});
 }
