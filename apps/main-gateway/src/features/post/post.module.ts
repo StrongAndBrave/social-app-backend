dd984d5c86bea91b2eda9d188b2d7e5ad2777bf3
TheBestPetr <ipetrazhitski@gmail.com>
2025-01-29 18:48:42 +0300

    add PostImagesRepository & PostImageSaveUseCase to post module

--- apps/main-gateway/src/features/post/post.module.ts
+++ apps/main-gateway/src/features/post/post.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { PostRepository } from './infrastructure/post.repository';
+import { PostRepository } from './infrastructure/posts/post.repository';
 import { CreatePostUseCase } from './application/use-cases/create.post.use-case';
 import { PostController } from './api/post.controller';
 import { DeletePostUseCase } from './application/use-cases/delete.post.use-case';
 import { PostUpdateUseCase } from './application/use-cases/update.post.use-case';
-import { PostQueryRepository } from './infrastructure/post.query.repository';
+import { PostQueryRepository } from './infrastructure/posts/post.query.repository';
 import { FilesClientService } from './application/files-microservice-connection/client-service';
 import { PostConfig } from './post.config';
+import { PostImagesRepository } from './infrastructure/posts-images/post-images.repository';
+import { PostImageSaveUseCase } from './application/use-cases/save.image.use-case';
 
 @Module({
 	imports: [JwtModule],
 	providers: [
+		{
+			provide: PostImagesRepository.name,
+			useClass: PostImagesRepository,
+		},
 		{
 			provide: PostRepository.name,
 			useClass: PostRepository,
@@ -26,6 +32,7 @@ import { PostConfig } from './post.config';
 		},
 		CreatePostUseCase,
 		PostUpdateUseCase,
+		PostImageSaveUseCase,
 		DeletePostUseCase,
 		FilesClientService,
 	],
