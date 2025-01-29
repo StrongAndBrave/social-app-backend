616df333f4386b189d59d3e03dbb5da21fc55d2d
TheBestPetr <ipetrazhitski@gmail.com>
2025-01-29 18:47:32 +0300

    fix postEntity

--- apps/main-gateway/src/features/post/domain/post.entity.ts
+++ apps/main-gateway/src/features/post/domain/post.entity.ts
@@ -4,12 +4,10 @@ import { PostCreateModel } from '../api/models/input/post.input';
 export class PostEntity extends BaseEntity {
 	userId: string;
 	description: string;
-	image: string;
 
 	constructor(postCreateData: PostCreateModel) {
 		super();
 		this.userId = postCreateData.userId;
 		this.description = postCreateData.description;
-		this.image = postCreateData.image;
 	}
 }
