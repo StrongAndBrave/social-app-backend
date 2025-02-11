65c5b5a98f7f06477a70acd5a7f2a3218905fb48
TheBestPetr <ipetrazhitski@gmail.com>
2025-02-11 16:02:32 +0300

    fix smtp

--- apps/main-gateway/src/core/adapters/mailer/mail.module.ts
+++ apps/main-gateway/src/core/adapters/mailer/mail.module.ts
@@ -4,58 +4,51 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { MailService } from './mail.service';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { join } from 'path';
-import { ConfigService } from '@nestjs/config';
+import { EmailConfigModule } from './email.config.module';
 
 @Module({
-  imports: [
-    MailerModule.forRootAsync({
-      inject: [ConfigService],
+	imports: [
+		MailerModule.forRootAsync({
+			inject: [EmailConfig],
+			imports: [EmailConfigModule],
+			useFactory: (emailConfig: EmailConfig) => {
+				return {
+					transport: {
+						service: emailConfig.mailerService,
+						secure: false,
+						auth: {
+							user: emailConfig.mailerLogin,
+							pass: emailConfig.mailerPassword,
+						},
+					},
 
-      // useFactory: (configService: ConfigService<any, true>) => {
-      //   const mailConfig = new EmailConfig(configService);
-      //   return {
-      //     transport: {
-      //       service: mailConfig.mailerService,
-      //       secure: false,
-      //       auth: {
-      //         user: mailConfig.mailerLogin,
-      //         pass: mailConfig.mailerPassword,
-      //       },
-      //     },
-
-          useFactory: (emailConfig: EmailConfig) => {
-            return {
-              transport: {
-                service: emailConfig.mailerService,
-                secure: false,
-                auth: {
-                  user: emailConfig.mailerLogin,
-                  pass: emailConfig.mailerPassword,
-                },
-              },
-
-          defaults: {
-            from: `Snapfolio <${emailConfig.mailerLogin}>`,
-          },
-          template: {
-            dir: join(process.cwd(), 'apps/main-gateway/src/core/adapters/mailer/templates'),
-            adapter: new HandlebarsAdapter(),
-            options: {
-              strict: true,
-            },
-          },
-        }
-      },
-    })
-  ],
-  providers: [
-    EmailConfig,
-    {
-      provide: MailService.name,
-      useClass: MailService,
-    },
-  ],
-  exports: [MailService.name],
+					defaults: {
+						from: `Snapfolio <${emailConfig.mailerLogin}>`,
+					},
+					template: {
+						dir: join(
+							process.cwd(),
+							'apps/main-gateway/src/core/adapters/mailer/templates',
+						),
+						adapter: new HandlebarsAdapter(),
+						options: {
+							strict: true,
+						},
+					},
+				};
+			},
+		}),
+	],
+	providers: [
+		{
+			provide: EmailConfig.name,
+			useClass: EmailConfig,
+		},
+		{
+			provide: MailService.name,
+			useClass: MailService,
+		},
+	],
+	exports: [MailService.name],
 })
-export class MailModule {
-}
+export class MailModule {}
