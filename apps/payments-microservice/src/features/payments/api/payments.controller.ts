0d312030e38bf30c88b4b595a692fef589d52193
TheBestPetr <ipetrazhitski@gmail.com>
2025-03-11 18:15:37 +0300

    add some logic

--- apps/payments-microservice/src/features/payments/api/payments.controller.ts
+++ apps/payments-microservice/src/features/payments/api/payments.controller.ts
@@ -1,10 +1,30 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
+import { CommandBus } from '@nestjs/cqrs';
+import { CreateStripePaymentCommand } from '../application/use-cases/create.stripe.payment.use-case';
 
-@Controller('payments')
+@Controller()
 export class PaymentsController {
-	constructor() {}
+	constructor(private commandBus: CommandBus) {}
 
-	@MessagePattern('Payments Controller')
-	async someDo() {}
+	@MessagePattern('create_payment')
+	async createPayment(data: {
+		userId: string;
+		paymentPeriod: 'day' | 'week' | 'month' | 'year';
+		paymentService: string;
+		amount: number;
+	}) {
+		console.log(data);
+		if (data.paymentService === 'STRIPE') {
+			console.log('data go to stripe payment');
+			await this.commandBus.execute(
+				new CreateStripePaymentCommand(
+					data.userId,
+					data.paymentPeriod,
+					data.paymentService,
+					data.amount,
+				),
+			);
+		}
+	}
 }
