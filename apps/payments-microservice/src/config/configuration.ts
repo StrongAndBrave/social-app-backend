8df9526f714c65a979e08e134c05cf1c4a409568
TheBestPetr <ipetrazhitski@gmail.com>
2025-03-11 18:12:43 +0300

    add data to payments core config

--- apps/payments-microservice/src/config/configuration.ts
+++ apps/payments-microservice/src/config/configuration.ts
@@ -15,5 +15,17 @@ export class CoreConfig {
 	@IsNotEmpty()
 	host: string = this.configService.get('HOST');
 
+	@IsNotEmpty()
+	stripeSecretKey: string = this.configService.get('STRIPE_SECRET_KEY');
+
+	@IsNotEmpty()
+	stripePublishKey: string = this.configService.get('STRIPE_PUBLISH_KEY');
+
+	@IsNotEmpty()
+	successPaymentResUrl: string = this.configService.get('SUCCESS_PAYMENT_RES_URL');
+
+	@IsNotEmpty()
+	failurePaymentResUrl: string = this.configService.get('FAILURE_PAYMENT_RES_URL');
+
 	constructor(private configService: ConfigService<any, true>) {}
 }
