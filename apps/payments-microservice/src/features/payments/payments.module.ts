81cdaaaff35e08f215aeeba4ee8a0d62c7f7ac02
TheBestPetr <ipetrazhitski@gmail.com>
2025-03-11 18:16:13 +0300

    rename & fix payments module

--- apps/payments-microservice/src/features/payments/payments.module.ts
+++ apps/payments-microservice/src/features/payments/payments.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './api/payments.controller';
-import { CoreConfig } from '../../config/configuration';
 import { ConfigModule } from '@nestjs/config';
 import { configModule } from '../../config/config-dynamic-module';
+import { CreateStripePaymentUseCase } from './application/use-cases/create.stripe.payment.use-case';
+import { CoreConfig } from '../../config/configuration';
+import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true }), configModule],
+	imports: [ConfigModule.forRoot({ isGlobal: true }), configModule, CqrsModule],
 	controllers: [PaymentsController],
-	providers: [CoreConfig],
+	providers: [CreateStripePaymentUseCase, CoreConfig],
 })
-export class PaymentsModule {}
+export class MicroservicePaymentsModule {}
