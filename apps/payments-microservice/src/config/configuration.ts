29c1c2f48bf2601573a70b03237e69f32fa3396b
TheBestPetr <ipetrazhitski@gmail.com>
2025-03-24 20:06:21 +0300

    add config & base entity

--- apps/payments-microservice/src/config/configuration.ts
+++ apps/payments-microservice/src/config/configuration.ts
@@ -22,6 +22,18 @@ export class CoreConfig {
 	stripePublishKey: string = this.configService.get('STRIPE_PUBLISH_KEY');
 
 	@IsNotEmpty()
+	daySubscriptionPrice: string = this.configService.get('DAY_SUBSCRIPTION_PRICE');
+
+	@IsNotEmpty()
+	weekSubscriptionPrice: string = this.configService.get('WEEK_SUBSCRIPTION_PRICE');
+
+	@IsNotEmpty()
+	monthSubscriptionPrice: string = this.configService.get('MONTH_SUBSCRIPTION_PRICE');
+
+	@IsNotEmpty()
+	yearSubscriptionPrice: string = this.configService.get('YEAR_SUBSCRIPTION_PRICE');
+
+	@IsNotEmpty()
 	successPaymentResUrl: string = this.configService.get('SUCCESS_PAYMENT_RES_URL');
 
 	@IsNotEmpty()
