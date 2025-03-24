5519ffeaed87838495f2f3ceaf99036c945d0c09
TheBestPetr <ipetrazhitski@gmail.com>
2025-03-24 20:08:47 +0300

    fix payments.controller.ts

--- apps/payments-microservice/src/features/payments/api/payments.controller.ts
+++ apps/payments-microservice/src/features/payments/api/payments.controller.ts
@@ -2,29 +2,26 @@ import { Controller } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 import { CommandBus } from '@nestjs/cqrs';
 import { CreateStripePaymentCommand } from '../application/use-cases/create.stripe.payment.use-case';
+import { PaymentInputModel } from './models/input/payment.input.model';
 
 @Controller()
 export class PaymentsController {
 	constructor(private commandBus: CommandBus) {}
 
 	@MessagePattern('create_payment')
-	async createPayment(data: {
-		userId: string;
-		paymentPeriod: 'day' | 'week' | 'month' | 'year';
-		paymentService: string;
-		amount: number;
-	}) {
+	async createPayment(data: PaymentInputModel) {
 		console.log(data);
 		if (data.paymentService === 'STRIPE') {
 			console.log('data go to stripe payment');
-			await this.commandBus.execute(
+			const paymentUrl = await this.commandBus.execute(
 				new CreateStripePaymentCommand(
 					data.userId,
+					data.username,
 					data.paymentPeriod,
 					data.paymentService,
-					data.amount,
 				),
 			);
+			return paymentUrl ?? null;
 		}
 	}
 }
