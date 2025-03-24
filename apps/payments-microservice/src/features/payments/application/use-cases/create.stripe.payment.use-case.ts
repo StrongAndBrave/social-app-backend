1fbd650710e7b05adcef49edc99f7e88f140345c
TheBestPetr <ipetrazhitski@gmail.com>
2025-03-24 20:09:33 +0300

    fix create.stripe.payment.use-case.ts

--- apps/payments-microservice/src/features/payments/application/use-cases/create.stripe.payment.use-case.ts
+++ apps/payments-microservice/src/features/payments/application/use-cases/create.stripe.payment.use-case.ts
@@ -5,9 +5,9 @@ import Stripe from 'stripe';
 export class CreateStripePaymentCommand {
 	constructor(
 		public userId: string,
+		public username: string,
 		public paymentPeriod: 'day' | 'week' | 'month' | 'year',
 		public paymentService: string,
-		public amount: number,
 	) {}
 }
 
@@ -18,32 +18,56 @@ export class CreateStripePaymentUseCase
 	constructor(private readonly coreConfig: CoreConfig) {}
 
 	async execute(command: CreateStripePaymentCommand) {
+		let amount = 0;
+
+		switch (command.paymentPeriod) {
+			case 'day':
+				amount = Number(this.coreConfig.daySubscriptionPrice);
+				break;
+			case 'week':
+				amount = Number(this.coreConfig.weekSubscriptionPrice);
+				break;
+			case 'month':
+				amount = Number(this.coreConfig.monthSubscriptionPrice);
+				break;
+			case 'year':
+				amount = Number(this.coreConfig.yearSubscriptionPrice);
+				break;
+		}
+
 		const stripe = new Stripe(this.coreConfig.stripeSecretKey, {
 			apiVersion: '2025-01-27.acacia',
 		});
 
-		const session = await stripe.checkout.sessions.create({
-			success_url: this.coreConfig.successPaymentResUrl,
-			cancel_url: this.coreConfig.failurePaymentResUrl,
-			line_items: [
-				{
-					price_data: {
-						unit_amount: command.amount * 100,
-						currency: 'USD',
-						product_data: {
-							name: 'Snapfolio',
-							description: 'Subscription for business account',
+		try {
+			const session = await stripe.checkout.sessions.create({
+				success_url: this.coreConfig.successPaymentResUrl,
+				cancel_url: this.coreConfig.failurePaymentResUrl,
+				line_items: [
+					{
+						price_data: {
+							unit_amount: amount * 100,
+							currency: 'USD',
+							product_data: {
+								name: 'Snapfolio',
+								description: `1 ${command.paymentPeriod} business account subscription for ${command.username}`,
+							},
+							recurring: {
+								interval: command.paymentPeriod,
+							},
 						},
-						recurring: {
-							interval: command.paymentPeriod,
-						},
+						quantity: 1,
 					},
-					quantity: 1,
-				},
-			],
-			mode: 'subscription',
-		});
-		console.log(session);
-		return session.url ?? null;
+				],
+				mode: 'subscription',
+				client_reference_id: command.userId,
+			});
+
+			console.log(session);
+			return session.url;
+		} catch (e) {
+			console.error(e);
+			throw e;
+		}
 	}
 }
