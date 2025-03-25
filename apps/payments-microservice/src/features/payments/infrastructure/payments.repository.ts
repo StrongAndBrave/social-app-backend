b9021189d5a0e473242bc27dccff69e281fa8cdb
TheBestPetr <ipetrazhitski@gmail.com>
2025-03-25 17:53:10 +0300

    fix payments repo

--- apps/payments-microservice/src/features/payments/infrastructure/payments.repository.ts
+++ apps/payments-microservice/src/features/payments/infrastructure/payments.repository.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../../../../../../libs/prisma/prisma.service';
-import { OrderEntity } from '../domain/order.entity';
 
 @Injectable()
 export class PaymentsRepository {
-	constructor(private readonly prisma: PrismaService) {}
+	constructor() {}
 
 	/*async createOrder(data: OrderEntity) {
 		return this.prisma.order.create({ data });*/
