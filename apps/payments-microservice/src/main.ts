bef48db04df08ae342c0c3382ff74a5fa9e764bb
TheBestPetr <ipetrazhitski@gmail.com>
2025-03-26 22:28:01 +0300

    add prefix

--- apps/payments-microservice/src/main.ts
+++ apps/payments-microservice/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions } from '@nestjs/microservices';
 import { CoreConfig } from './config/configuration';
 import { AppModule } from './app.module';
+import { configApp } from './config/apply-app-settings/set.app';
 
 async function bootstrap() {
 	const appContext = await NestFactory.createApplicationContext(AppModule);
 	const coreConfig = appContext.get<CoreConfig>(CoreConfig);
 
 	const app = await NestFactory.create(AppModule);
+	configApp(app);
 	await app.listen(coreConfig.httpPort);
 	console.log(`Payments Microservice HTTP is listening on port:${coreConfig.httpPort}`);
 
