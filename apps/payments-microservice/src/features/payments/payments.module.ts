f4e817a346a2a095bb0206a9075f108ef317bc76
TheBestPetr <ipetrazhitski@gmail.com>
2025-03-28 17:36:31 +0300

    rm coreModule from paymentsModule

--- apps/payments-microservice/src/features/payments/payments.module.ts
+++ apps/payments-microservice/src/features/payments/payments.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { PaymentsController } from './api/payments.controller';
 import { CreateStripePaymentUseCase } from './application/use-cases/create.stripe.payment.use-case';
 import { PaymentsConfig } from './payments.config';
-import { CoreConfig } from '../../config/configuration';
 
 @Module({
 	imports: [],
 	controllers: [PaymentsController],
-	providers: [CreateStripePaymentUseCase, PaymentsConfig, CoreConfig],
+	providers: [CreateStripePaymentUseCase, PaymentsConfig],
 })
 export class MicroservicePaymentsModule {}
