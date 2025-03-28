f5d9471807f6614cec0d8044bcc5f46be3f883d6
TheBestPetr <ipetrazhitski@gmail.com>
2025-03-28 18:29:08 +0300

    add dbConfig & dbConfigModule & dbModule

--- apps/payments-microservice/src/app.module.ts
+++ apps/payments-microservice/src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { CoreModule } from './core/core.module';
 import { configModule } from './config/config-dynamic-module';
 import { MicroservicePaymentsModule } from './features/payments/payments.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-	imports: [CoreModule, configModule, MicroservicePaymentsModule],
+	imports: [CoreModule, configModule, MicroservicePaymentsModule, DatabaseModule],
 })
-
 export class AppModule {}
