b36cb88aed1be3f2094fe657f90c25e01f54db21
TheBestPetr <ipetrazhitski@gmail.com>
2025-03-28 19:17:41 +0300

    add FinishSubscriptionUseCase & FailureSubscriptionUseCase

--- apps/payments-microservice/src/features/payments/payments.module.ts
+++ apps/payments-microservice/src/features/payments/payments.module.ts
@@ -6,13 +6,18 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Subscription } from './domain/subscription.entity';
 import { CreateSubscriptionUseCase } from './application/use-cases/subscriptions/create.subscription.use-case';
 import { SubscriptionRepository } from './infrastructure/subscription.repository';
+import { FinishSubscriptionUseCase } from './application/use-cases/subscriptions/finish.subscription.use-case';
+import { FailureSubscriptionUseCase } from './application/use-cases/subscriptions/failure.subscription.use-case';
+import { WebhookController } from './api/webhook.controller';
 
 @Module({
 	imports: [SequelizeModule.forFeature([Subscription])],
-	controllers: [PaymentsController],
+	controllers: [PaymentsController, WebhookController],
 	providers: [
 		CreateStripePaymentUseCase,
 		CreateSubscriptionUseCase,
+		FinishSubscriptionUseCase,
+		FailureSubscriptionUseCase,
 		SubscriptionRepository,
 		PaymentsConfig,
 	],
