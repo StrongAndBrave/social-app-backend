c4380f5fffbaea19eb6099c307ad17fd6d173702
TheBestPetr <ipetrazhitski@gmail.com>
2025-03-28 17:38:45 +0300

    + / - files

--- apps/payments-microservice/src/features/payments/infrastructure/paymentsRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../../../../../../libs/prisma/prisma.service';
-
-@Injectable()
-export class PaymentsRepository {
-	constructor(private readonly prismaService: PrismaService) {}
-}
