48a2d38baf2216857761553ae4d891be20c43ea2
TheBestPetr <ipetrazhitski@gmail.com>
2025-03-28 19:17:24 +0300

    add WebhookController

---
+++ apps/payments-microservice/src/features/payments/api/webhook.controller.ts
@@ -0,0 +1,43 @@
+import { Body, Controller, HttpCode, Post, Req } from '@nestjs/common';
+import { PaymentsConfig } from '../payments.config';
+import Stripe from 'stripe';
+import { CommandBus } from '@nestjs/cqrs';
+import { FinishSubscriptionCommand } from '../application/use-cases/subscriptions/finish.subscription.use-case';
+import { FailureSubscriptionCommand } from '../application/use-cases/subscriptions/failure.subscription.use-case';
+
+@Controller('payments')
+export class WebhookController {
+	constructor(
+		private readonly commandBus: CommandBus,
+		private readonly paymentsConfig: PaymentsConfig,
+	) {}
+
+	@Post('stripe-webhook')
+	@HttpCode(200)
+	async stripeWebhook(@Body() data: any, @Req() req: Request) {
+		const stripe = new Stripe(this.paymentsConfig.stripeSecretKey, {
+			apiVersion: '2025-01-27.acacia',
+		});
+		const signature = req.headers['stripe-signature'];
+		try {
+			const event = stripe.webhooks.constructEvent(
+				data,
+				signature,
+				this.paymentsConfig.stripeWebhookSecretKey,
+			);
+			if (event.type === 'checkout.session.completed') {
+				const session = event.data.object as Stripe.Checkout.Session;
+				await this.commandBus.execute(
+					new FinishSubscriptionCommand(session.client_reference_id!),
+				);
+			} else {
+				const session = event.data.object as Stripe.Checkout.Session;
+				await this.commandBus.execute(
+					new FailureSubscriptionCommand(session.client_reference_id!),
+				);
+			}
+		} catch (e) {
+			console.error(e);
+		}
+	}
+}
