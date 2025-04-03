1c1b8f735c950de660a9148eda9ab13dcecc8427
TheBestPetr <ipetrazhitski@gmail.com>
2025-04-03 18:12:45 +0300

    add http connection

--- apps/payments-microservice/src/main.ts
+++ apps/payments-microservice/src/main.ts
@@ -1,16 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions } from '@nestjs/microservices';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { CoreConfig } from './config/configuration';
 import { AppModule } from './app.module';
+import { configApp } from './config/apply-app-settings/set.app';
 
 async function bootstrap() {
-	const appContext = await NestFactory.createApplicationContext(
-		AppModule,
-	);
+	const appContext = await NestFactory.createApplicationContext(AppModule);
 	const coreConfig = appContext.get<CoreConfig>(CoreConfig);
-	const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+
+	const app = await NestFactory.create(AppModule);
+	configApp(app);
+	await app.listen(coreConfig.httpPort);
+	console.log(`Payments Microservice HTTP is listening on port ${coreConfig.httpPort}`);
+
+	const microservice = await NestFactory.createMicroservice<MicroserviceOptions>(
 		AppModule,
 		{
+			transport: Transport.TCP,
 			options: {
 				host: coreConfig.tcpHost,
 				port: coreConfig.tcpPort,
@@ -18,8 +24,8 @@ async function bootstrap() {
 		},
 	);
 
-	await app.listen();
-	console.log(`Payments Microservice is listening on port ${coreConfig.tcpPort}`);
+	await microservice.listen();
+	console.log(`Payments Microservice TCP is listening on port ${coreConfig.tcpPort}`);
 }
 
 /* {
