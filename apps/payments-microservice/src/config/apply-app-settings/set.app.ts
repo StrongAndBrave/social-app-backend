c9338504be69b574c38ff10ca61487c1eb060b63
TheBestPetr <ipetrazhitski@gmail.com>
2025-04-07 17:44:12 +0300

    add rawData to webhook

--- apps/payments-microservice/src/config/apply-app-settings/set.app.ts
+++ apps/payments-microservice/src/config/apply-app-settings/set.app.ts
@@ -1,7 +1,9 @@
 import { INestApplication } from '@nestjs/common';
+import * as express from 'express';
 
 export const GLOBAL_PREFIX = 'api/v1';
 
 export function configApp(app: INestApplication) {
 	app.setGlobalPrefix(GLOBAL_PREFIX);
+	app.use('/api/v1/payments/stripe-webhook', express.raw({ type: 'application/json' }));
 }
