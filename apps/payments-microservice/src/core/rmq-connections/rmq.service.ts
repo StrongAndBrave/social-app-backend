5b9ab928464a25b8d8b32e79452e24c36c1f53d0
TheBestPetr <ipetrazhitski@gmail.com>
2025-05-30 12:15:14 +0300

    fix data to send in rmq

--- apps/payments-microservice/src/core/rmq-connections/rmq.service.ts
+++ apps/payments-microservice/src/core/rmq-connections/rmq.service.ts
@@ -5,10 +5,16 @@ import { ClientProxy } from '@nestjs/microservices';
 export class RmqService {
 	@Inject('RMQ') private rmqProxyClient: ClientProxy;
 
-	async sendSubscriberData(data: { userId: string; isSubscribed: boolean }) {
+	async sendSubscriberData(data: {
+		userId: string;
+		accountType: 'Business';
+		subscriptionPeriod: string;
+	}) {
 		try {
 			console.log(data);
-			return await this.rmqProxyClient.send({ cmd: 'send_data' }, data).toPromise();
+			return await this.rmqProxyClient
+				.send({ cmd: 'upgradeAccountType' }, data)
+				.toPromise();
 		} catch (error) {
 			console.error('something wrong with send data', error);
 			return null;
